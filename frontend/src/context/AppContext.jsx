c4f6e87f007024f546c40fec7ca6b8521357ad0f
Ankit kumar <169060244+Ankitkumar445@users.noreply.github.com>
2026-07-05 18:21:50 +0530

    Change backend URL in AppContext
    Updated backend URL to a specific deployment address.

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext=createContext()
 export const AppContextProvider =(props)=>{
 
     axios.defaults.withCredentials=true
-    const backendUrl= import.meta.env.VITE_BACKEND_URL
+    const backendUrl= "https://mern-authentication-backend-tnnj.onrender.com"
     const [isLoggedin, setIsLoggedin]=useState(false)
     const [userData, setUserData]= useState(false)
 
